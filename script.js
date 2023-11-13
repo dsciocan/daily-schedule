79eeac1d484c2c3b7ac56cfbd9329d10ed285a2e
Daniela <daniela.ciocan137@gmail.com>
2023-11-13 22:54:35 +0200

    Code for background change based on current time

--- script.js
+++ script.js
@@ -3,14 +3,14 @@
 // Display the current day at the top of the calender when a user opens the planner.
 var currentDay = $('#currentDay');
 currentDay.text(dayjs().format('DD MMM YYYY'));
-var times = [9,10,11,12,1,2,3,4,5];
+var times = [9,10,11,12,13,14,15,16,17];
 var timeBlock = $(".container")
 timeBlock.addClass("time-block")
 // Present timeblocks for standard business hours when the user scrolls down.
 for(i=0; i<times.length; i++) {
 var row = $('<div>').addClass('row g-0');
 var timeDiv = $('<div>').addClass('col-lg-1 hour');
-var timeSlot = $('<label>').text(times[i]);
+var timeSlot = $('<label>').text(times[i] + ":00");
 timeDiv.append(timeSlot);
 var inputDiv = $('<div>').addClass('col-lg-9');
 var inputField = $('<textarea class="form-control" aria-label="With textarea" rows="3"></textarea>');
@@ -20,6 +20,7 @@ inputDiv.append(inputField);
 buttonDiv.append(saveButton);
 row.append(timeDiv, inputDiv, buttonDiv);
 timeBlock.append(row);
+currentTime();
 }
 
 
@@ -29,12 +30,19 @@ timeBlock.append(row);
 
 //link to function that validates whether the time has passed, is now or is later and changes background color accordingly
 function currentTime() {
-    
+   var timeNow = Number(dayjs().format("H"));
+   if (timeNow < times[i]) {
+    inputField.addClass('future');
+    $("textarea.form-control").click(function () {
+        $(this).addClass("future");
+    });
+   } else if(timeNow == times[i]) {
+    inputField.addClass('present');
+   } else {
+    inputField.replaceWith('<textarea class="form-control past" type="text" placeholder="" rows="3" readonly></textarea>')
 }
-//based on time, delete input field and replace it with p 
 
-
-
+}
 
 
 // Color-code each timeblock based on past, present, and future when the timeblock is viewed.
